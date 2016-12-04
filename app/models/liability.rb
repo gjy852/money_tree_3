5df6b9d623f8efac631bda449f384270eedff9c0
Raghu Betina <raghu@firstdraft.com>
2016-12-04 22:30:10 +0000

    Direct association between liability_types and liabilities

--- app/models/liability.rb
+++ app/models/liability.rb
@@ -1,6 +1,9 @@
 class Liability < ApplicationRecord
   # Direct associations
 
+  belongs_to :liabilities_type,
+             :class_name => "LiabilityType"
+
   belongs_to :net_worth
 
   # Indirect associations
--- app/models/liability_type.rb
+++ app/models/liability_type.rb
@@ -1,6 +1,10 @@
 class LiabilityType < ApplicationRecord
   # Direct associations
 
+  has_one    :liability,
+             :foreign_key => "liabilities_type_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
