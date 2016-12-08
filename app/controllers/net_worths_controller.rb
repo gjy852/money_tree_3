12813a7579ef443897f5eea8c3af25f9b9273b89
Grace Yeh <gyeh@users.noreply.github.com>
2016-12-07 20:04:46 -0600

    Net worth index (A+L)

--- app/controllers/net_worths_controller.rb
+++ app/controllers/net_worths_controller.rb
@@ -8,6 +8,8 @@ class NetWorthsController < ApplicationController
     @q = NetWorth.ransack(params[:q])
     @net_worths = @q.result(:distinct => true).includes(:liabilities, :assets, :user).page(params[:page]).per(10)
     @calculate_asset_total = Asset.sum(:asset_value)
+    @calculate_liability_total = Liability.sum(:liabilities_value)
+    @calculate_net_worth = @calculate_asset_total.to_i + @calculate_liability_total.to_i
 
     render("net_worths/index.html.erb")
   end
